b66d683ee0ee8c25cfd2b8b3ea7b8fe165039481
Skyler <skylerseo.dev@gmail.com>
2021-09-02 04:17:27 +0900

    MobileMenu.jsx a태그 제거

--- src/components/Nav/Sections/MobileMenu.jsx
+++ src/components/Nav/Sections/MobileMenu.jsx
@@ -4,11 +4,9 @@ import menu_mobile from '../../../assets/icon/menu_white.svg'
 
 const MobileMenu = (props) => {
   return (
-    <a onClick={props.showDrawerHandler}>
-      <div className='menu_mobile'>
-        {props.isVisible ? <img src={close_mobile} alt='메뉴 닫기' /> : <img src={menu_mobile} alt='메뉴 열기' />}
-      </div>
-    </a>
+    <div className='menu_mobile' onClick={props.showDrawerHandler}>
+      {props.isVisible ? <img src={close_mobile} alt='메뉴 닫기' /> : <img src={menu_mobile} alt='메뉴 열기' />}
+    </div>
   )
 }
 
